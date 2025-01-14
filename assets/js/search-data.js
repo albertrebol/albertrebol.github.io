8cfcecf9bc2e4a1b3b0442cb6b5a0fa3a254513a
albertrebol <albert.congliu@qq.com>
2025-01-14 01:50:44 +0000

    Deploying to gh-pages from @ albertrebol/albertrebol.github.io@014aef73102a0015cf55626550699397ae1f7fec 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -457,14 +457,14 @@ ninja.data = [{
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
+          window.open("mailto:%61%6C%62%65%72%74.%63%6F%6E%67%6C%69%75@%71%71.%63%6F%6D", "_blank");
         },
       },{
-        id: 'social-inspire',
-        title: 'Inspire HEP',
+        id: 'social-orcid',
+        title: 'ORCID',
         section: 'Socials',
         handler: () => {
-          window.open("https://inspirehep.net/authors/1010907", "_blank");
+          window.open("https://orcid.org/0009-0003-8988-696X", "_blank");
         },
       },{
         id: 'social-rss',
@@ -474,18 +474,11 @@ ninja.data = [{
           window.open("/feed.xml", "_blank");
         },
       },{
-        id: 'social-scholar',
-        title: 'Google Scholar',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
-        },
-      },{
         id: 'social-custom_social',
         title: 'Custom_social',
         section: 'Socials',
         handler: () => {
-          window.open("https://www.alberteinstein.com/", "_blank");
+          window.open("https://albertrebol.github.io/", "_blank");
         },
       },{
       id: 'light-theme',
